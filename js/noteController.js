da3fbc94399f21b70c683fdad63ed8a3d2ee1fbd
nik <vincent.tesla@gmail.com>
2015-07-28 15:03:01 +0800

    add 3 js file,update html and css file

---
+++ js/noteController.js
@@ -0,0 +1,205 @@
+/**overflowteam
+* FlowNote 交互逻辑实现
+* creat by overflow-inherit on 2015.7.26
+*/
+var bookList_view = q(".J_Book_list");//笔记本列表
+var book_insert = q(".J_Insert_book");//确定按钮（input后面）
+var bookinsert_input = q(".J_Insert_book input");//新建笔记本的命名输入框
+var newbook_button = q(".J_New_book");//新建笔记本按钮
+var delbook_button = q(".J_Del_book");//删除笔记本按钮
+var noteList_view = q(".J_Note_list ul");//笔记列表
+var article_input = q(".article input");//新建笔记的标题输入框
+var article_text = q(".article textarea");//新建笔记的内容输入框
+var right_content = q(".J_Right_content");//右侧笔记详情模块
+var right_title = q(".J_Note_title");//右侧笔记标题
+var delnote_button = q(".J_Del_note");//右侧笔记删除按钮
+var note_content = q(".J_Note_content");//右侧笔记内容
+var bookActiveId = "";//被选中的笔记本id（String）
+var noteActiveId = "";//被选中的笔记id（tring）
+var canChangeTitle = true;
+var canChangeContent = false;
+
+//notebooks [{"book_id":"1","book_name":"javascript","book_notenumber":"0"},......]
+//展示所有的笔记本
+function showAllBooks(){
+    var bookInfos = "";
+    for(key in notebookJson){
+    	var name = notebookJson[key].book_name;
+    	var number = notebookJson[key].book_notenumber;
+    	var id = notebookJson[key].book_id;
+        bookInfos += "<li class=\"book-item\" id=\"book"+id+"\"><h3>"+name+"<span>("+number+")</span></h3></li>";
+	}
+	bookList_view.innerHTML = bookInfos;
+	$("#"+bookActiveId).addClass("book-item-on");
+}
+
+//添加一个笔记本
+function addOneBook(name){
+	addNotebook(name);
+	showAllBooks();
+}
+
+//删除选中的笔记本
+function deleteOneBook(){
+	if(confirm("是否删除选中的笔记本？")){
+	    var id = strToInt(/^book/,bookActiveId);
+	    removeNotebook(id);
+	    bookActiveId = "";
+	    showAllBooks();
+    }
+}
+
+//notes [{"note_id":"1","note_name":"javascript作用域","book_id":"1","text":"任何程序设计语言都有作用域的概念，简单的说，作用域就是变量与函数的可访问范围，即作用域控制着变量与函数的可见性和生命周期。在JavaScript中，变量的作用域有全局作用域和局部作用域两种。"},......]
+//展示笔记简述
+function showNoteAb(){
+	var noteInfos = "";
+	if(bookActiveId != ""){
+	    var id = strToInt(/^book/,bookActiveId);
+	    var notesArr = searchNotesById(id);
+        for(key in notesArr){
+        	var h1 = notesArr[key].note_name;
+        	var noteId = notesArr[key].note_id;
+        	var abstract = (notesArr[key].text).slice(0,100);
+        	noteInfos += "<li class=\"note-item J_Note_item\" id=\"note"+noteId+"\"><h3>"+h1+"</h3><p>"+abstract+"</p></li>";
+        }
+	}
+	noteList_view.innerHTML = noteInfos;
+	right_content.style.display = "none";
+}
+
+//展示笔记详情
+function showNote(){
+	var id = strToInt(/^note/,noteActiveId);
+	var oneNote = searchNoteDetialByid(id);
+	right_title.innerHTML = oneNote.name;
+	note_content.value = oneNote.text;
+}
+
+//添加一个笔记
+function addOneNote(name,content){
+    addNote(strToInt(/^book/,bookActiveId),name,content);
+}
+
+//删除选中的笔记
+function deleteOneNote(){
+	if(confirm("是否删除选中的笔记？")){
+	    var id = strToInt(/^note/,noteActiveId);
+	    var bookId = strToInt(/^book/,bookActiveId);
+	    removeNote(bookId,id);
+	    noteActiveId = "";
+	    showNoteAb();
+	    showAllBooks();
+    }
+}
+
+//修改笔记标题
+function changeNoteTitle(title){
+	var id = strToInt(/^note/,noteActiveId);
+	updateNoteName(id,title);
+	showNoteAb();
+	$("#"+noteActiveId).addClass("note-item-on");
+	right_content.style.display = "block";
+	showNote();
+}
+
+//修改笔记内容
+function changeNoteContent(content){
+	var id = strToInt(/^note/,noteActiveId);
+	updateNoteText(id,content);
+	showNoteAb();
+	$("#"+noteActiveId).addClass("note-item-on");
+	right_content.style.display = "block";
+	showNote();
+}
+
+//将字符串id转化为数字id
+function strToInt(regex,idstr){
+	return parseInt(idstr.replace(regex,""));
+}
+
+window.onload = function(){
+showAllBooks();
+q.on(".J_New_book","click",function(){
+	book_insert.style.zIndex = "2000";
+	bookinsert_input.value = "";
+});
+q.on(".J_Insert_button","click",function(){
+	var newName = bookinsert_input.value;
+	if(newName == "" || newName == null){
+		alert("您还没有给新的笔记本起名!");
+	}else {
+		addOneBook(newName);
+		book_insert.style.zIndex = "0";
+	}
+});
+q.on(".J_Del_book","click",function(){
+	if(bookActiveId != "")
+        deleteOneBook();
+    else
+    	alert("请选择要删除的笔记本！");
+});
+q.delegate(".J_Book_list","li","click",function(){
+	$(".book-item-on").removeClass("book-item-on");
+	$(this).addClass("book-item-on");
+	bookActiveId = this.id;
+	showNoteAb();
+});
+q.on(".J_New_note","click",function(){
+	article_input.value = "";
+	article_text.value = "";
+	if (bookActiveId != "") {
+        q(".article_wraper").style.display = "block";
+	}else{
+		alert("请先选择笔记本！");
+	}
+});
+q.on("#artsavebutton","click",function(){
+  	var h1 = article_input.value;
+  	var content = article_text.value ? article_text.value : "";
+  	if(h1 == "" || h1 == null){
+  		alert("请先输入标题再保存!");
+  	}else{
+  		addOneNote(h1,content);
+ 		showAllBooks();
+  		showNoteAb();
+  		q(".article_wraper").style.display = "none";
+  	}
+});
+q.on("#artcancelbutton","click",function(){
+    q(".article_wraper").style.display = "none";
+});
+q.delegate(".J_Note_list ul","li","click",function(){
+    $(".note-item-on").removeClass("note-item-on");
+	$(this).addClass("note-item-on");
+	right_content.style.display = "block";
+	noteActiveId = this.id;
+	showNote();
+});
+q.on(".J_Del_note","click",function(){
+	deleteOneNote();
+	right_content.style.display = "none";
+});
+q.on(".J_Note_title","click",function(){
+	if (canChangeTitle) {
+	    var before = right_title.innerHTML
+	    right_title.innerHTML = "<input type=\"text\" value=\""+before+"\">";
+	    q(".J_Note_title input").focus();
+	    q(".J_Note_title input").select();
+	    canChangeTitle = false;
+	    q.on(".J_Note_title input","blur",function(){
+	var after = q(".J_Note_title input").value;
+	if (after == null || after == "") {
+		alert("标题不能为空！");
+		q(".J_Note_title input").focus();
+	}else{
+		changeNoteTitle(after);
+		canChangeTitle = true;
+	}
+});
+	}
+});
+q.on(".J_Note_content","blur",function(){
+	var after = note_content.value;
+		changeNoteContent(after);
+});
+}
---
+++ js/noteDao.js
@@ -0,0 +1,159 @@
+/**overflowteam
+* FlowNote 数据操作
+* creat by overflow-inherit on 2015.7.26
+*/
+//[{"book_id":"1","book_name":"javascript学习指南"，"book_notenumber":"1"},{"book_id":"2","book_name":"javascript编程精解","book_notenumber":"0"}]
+//[{"note_id":"1","note_name":"javascript作用域","book_id":"1","text":"任何程序设计语言都有作用域的概念，简单的说，作用域就是变量与函数的可访问范围，即作用域控制着变量与函数的可见性和生命周期。在JavaScript中，变量的作用域有全局作用域和局部作用域两种。"},......]
+var nbl = "notebookList";
+var notebookJson = q.readStorageJson(nbl);//笔记本数组
+var nl = "noteList";
+var noteJson = q.readStorageJson(nl);//笔记数组
+
+//一键初始化(用于测试)
+function initial(){
+	notebookJson = [];
+	noteJson = [];
+	q.storageJson(nbl,notebookJson);
+	q.storageJson(nl,noteJson);
+}
+
+//添加笔记本
+function addNotebook(name){
+	var notebookId;
+	var notbookObj;
+    if(localStorage.getItem(nbl) != null && localStorage.getItem(nbl) != "[]"){
+        notebookId = getNewNotebookId();
+    }else {
+        notebookId = 1;
+        q.storageJson(nbl,new Array());
+    }
+    notbookObj = {"book_id":notebookId,"book_name":name,"book_notenumber":0};
+    notebookJson.push(notbookObj);
+    q.storageJson(nbl,notebookJson);
+}
+
+//删除笔记本
+function removeNotebook(id){
+	 for(key in notebookJson){
+    	if (notebookJson[key].book_id == id) {
+    		notebookJson.splice(key,1);
+	        q.storageJson(nbl,notebookJson);
+    	};
+    }
+}
+
+//取得新增笔记本的id
+function getNewNotebookId(){
+	return notebookJson[notebookJson.length - 1].book_id + 1;
+}
+//取得新增笔记的id
+function getNewNoteId(){
+	return noteJson[noteJson.length - 1].note_id + 1;
+}
+
+//查询对应id的笔记本的所有笔记
+function searchNotesById(id){
+    var arr = [];
+    for(key in noteJson){
+    	if(noteJson[key].book_id == id){
+    		arr.push(noteJson[key]);
+    	}
+    }
+    return arr;
+}
+
+//在指定id的笔记本下创建新笔记
+function addNote(bookId,name,content){
+	var noteId;
+	var noteObj;
+    if(localStorage.getItem(nl) != null && localStorage.getItem(nl) != "[]"){
+        noteId = getNewNoteId();
+    }else {
+        noteId = 1;
+        q.storageJson(nl,new Array());
+    }
+    noteObj = {"note_id":noteId,"note_name":name,"book_id":bookId,"text":content};
+    noteJson.push(noteObj);
+    q.storageJson(nl,noteJson);
+    addNoteNumber(bookId);
+}
+
+//指定id的笔记本的笔记数量加一
+function addNoteNumber(bookId){
+	for(key in notebookJson){
+		if(notebookJson[key].book_id == bookId){
+			notebookJson[key].book_notenumber++;
+			break;
+		}
+	}
+	q.storageJson(nbl,notebookJson);
+}
+
+//指定id的笔记本的笔记数量减一
+function minusNoteNumber(bookId){
+	for(key in notebookJson){
+		if(notebookJson[key].book_id == bookId){
+			notebookJson[key].book_notenumber--;
+			break;
+		}
+	}
+	q.storageJson(nbl,notebookJson);
+}
+
+//取得noteNumber
+function getNoteNumber(id){
+	var number = 0;
+	for(key in noteJson){
+		if(noteJson[key].book_id == id){
+    		number++;
+    	}
+    }
+    return number;
+}
+
+//查询指定id的笔记详情
+function searchNoteDetialByid(id){
+	var oneNote = {};
+	for(key in noteJson){
+		if (noteJson[key].note_id == id) {
+            oneNote.name = noteJson[key].note_name;
+            oneNote.text = noteJson[key].text;
+            break;
+		}
+	}
+	return oneNote;
+}
+
+//删除目标笔记
+function removeNote(bookId,id){
+    for(key in noteJson){
+    	if (noteJson[key].note_id == id) {
+    		noteJson.splice(key,1);
+	        q.storageJson(nl,noteJson);
+	        break;
+    	};
+    }
+    minusNoteNumber(bookId);
+}
+
+//修改目标笔记的标题
+function updateNoteName(noteId,name){
+	  for(key in noteJson){
+    	if (noteJson[key].note_id == noteId) {
+    		noteJson[key].note_name = name;
+	        q.storageJson(nl,noteJson);
+	        break;
+    	};
+    }
+}
+
+//修改目标笔记的内容
+function updateNoteText(id,text){
+	for(key in noteJson){
+    	if (noteJson[key].note_id == id) {
+    		noteJson[key].text = text;
+	        q.storageJson(nl,noteJson);
+	        break;
+    	};
+    }
+}
